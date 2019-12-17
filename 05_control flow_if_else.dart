4c72d75230fb9c4c268ed1fd01e527fd9952d102
Chetanya Patil <55090063+Chetanya-Patil@users.noreply.github.com>
2019-12-17 18:00:57 +0530

    Control flow_if_else

---
+++ 05_control flow_if_else.dart
@@ -0,0 +1,35 @@
+//control flow statements//if and else
+
+void main()
+{
+  //if and  else 
+  
+  var salary = 25000;
+  
+  if (salary > 20000)
+  {
+     print("You got promotion .congrutualation");
+    
+  }
+  else
+  {
+    print("You need to work hard");
+    
+  }
+  
+  //if else if ladder
+  
+  var marks = 70;
+   
+  if (marks >= 90 && marks < 100)
+  {
+    print("A+ grade");
+    
+  }else if (marks >= 80 && marks < 90){
+    print("B+ grade ");
+  }else 
+  {
+    print("Invalid marks");
+  }
+  
+}
