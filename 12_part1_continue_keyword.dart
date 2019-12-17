51363b0296e0450032c28b10f2935c380798cadd
Chetanya Patil <55090063+Chetanya-Patil@users.noreply.github.com>
2019-12-17 18:08:41 +0530

    part1_continue_keyword

---
+++ 12_part1_continue_keyword.dart
@@ -0,0 +1,13 @@
+// PART 1 : continue keyword 
+
+void main()
+{
+  for ( int i = 0 ; i <= 10; i++)
+  {
+    if ( i % 2 == 0) //it will show the odd numbers
+    {
+      continue;
+    }
+    print(i);
+  }
+}
