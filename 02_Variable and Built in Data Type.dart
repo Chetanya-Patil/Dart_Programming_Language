eb646519deafbbff66bf93105bbbb02b349f6470
Chetanya Patil <55090063+Chetanya-Patil@users.noreply.github.com>
2019-12-17 17:59:32 +0530

    Variable and Built in Data Type

---
+++ 02_Variable and Built in Data Type.dart
@@ -0,0 +1,34 @@
+//Declearation of variable
+//Built in data type
+
+void main() {
+  //Number : int
+
+  int age = 10;
+  var old = 10; //it is inferred as integer automatically
+  int hexValue = 0xEADEBAEE;
+
+  //Numbers: double
+
+  double percentage = 93.4;
+  var percent = 82.55333;
+  double exponents = 1.42e5;
+
+  //String
+
+  String name = "henry";  //  Here in String keyword first letter  is in uppercase foramt 
+  var company = "google";
+
+  //Boolean
+
+  bool isValid = true;
+  var isAlive = false;
+
+  print(age);
+  print(old);
+  print(name);
+  print(company);
+
+  // Note: All data types in Dart are Objects.
+//        Therefore ,their initial value is by default 'null'.
+}
