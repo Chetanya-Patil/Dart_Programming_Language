619aa133f458207f22660ff35660db0d0660f9c1
Chetanya Patil <55090063+Chetanya-Patil@users.noreply.github.com>
2019-12-17 18:06:36 +0530

    Switch_Case

---
+++ 07_Switch_Case.dart
@@ -0,0 +1,28 @@
+void main()
+{
+  //Switch case statements : Application for only 'int' and 'string'
+  
+  String grade = 'A';
+  
+  switch (grade)
+  {
+    case 'A':
+      print("Exellent grade of A");
+      break;
+      
+    case 'B':
+      print("very good!");
+      break;
+      
+    case 'C':
+      print("good!");
+      break;
+      
+    default:
+      print("YOU HAVE FAILED ");
+      
+      
+      
+      
+  }
+}
