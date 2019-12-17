c1598537515fb0a273e87dac481e5b8b2ab82e20
Chetanya Patil <55090063+Chetanya-Patil@users.noreply.github.com>
2019-12-17 18:00:27 +0530

    Final and Const keyword

---
+++ 04_Final and Const keyword.dart
@@ -0,0 +1,22 @@
+//Defining constants : 'final' and 'const' keyword 
+void main()
+{
+//final keyword 
+
+ final cityName = "mumbai";
+ //cityName ="delhi";
+
+ final String countryName = 'india';
+
+
+//const keyword 
+  const PI = 3.14;
+  const double gravity = 9.8;
+
+}
+
+class Circle{
+    final color = "Red";
+    static const PI = 3.14;
+  
+}
