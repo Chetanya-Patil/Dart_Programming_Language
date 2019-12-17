081480772aa9e1552d1300a3416404dfd198f8a6
Chetanya Patil <55090063+Chetanya-Patil@users.noreply.github.com>
2019-12-17 18:07:01 +0530

    For_Loop

---
+++ 08_For_Loop.dart
@@ -0,0 +1,31 @@
+//loops 
+
+
+void main()
+{
+  //way to find tthe even number between 1 to 10
+  
+  for (int i = 0 ; i < 4 ; i++)
+  {
+    if( i % 2 == 0)
+    {
+      print(i);
+    }
+  }
+  
+  //for  in loop
+  
+  List planetlist = ["Mercury","Venus","Earth","Mars"];
+  
+  for (String planet in planetlist)
+  {
+    print(planet);
+  }
+  
+  
+  
+  
+  
+  
+  
+}
